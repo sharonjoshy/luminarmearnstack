8b1498fe037acd6d11af6e82f84dda6141dd0059
Sharon <sharonjoshys56@gmail.com>
2021-08-01 21:27:20 +0530

    javasc

--- java script/script.js
+++ java script/script.js
@@ -1,6 +1,14 @@
-var name="Arjun",age=25;
+var company_name="luminar"
+var company_location="kakkanad"
+// our company luminar located in kakkanad
+
+// console.log("our company"+company_name+"located in"+company_location);
+
+
+// or
+
+console.log(`our company ${company_name} located in ${company_location}`);
+
+// hi iam arjun here 28 years old
+var name="arjun",age="25"
 console.log(`hi iam ${name} here ${age} years old`);
-var company_name="luminar tech"
-// companuy name luminar technolab
-console.log("company name is"+company_name);
-console.log("company name is" ,company_name);
